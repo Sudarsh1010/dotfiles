e5c9c2daf61768378b7074cde84038e7e741bce9
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-10-28 14:19:47 +0530

    fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,30 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# List Directory
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+pyenv init - | source
+
+set -xg PYTHONPATH $HOME/workspace/predigle/pluto/api-pluto-pmi $PYTHONPATH
